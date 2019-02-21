7ec16637a5498c2248b29515e85c7f1ea077feca
lrnavarro <linmery.navarro@gmail.com>
2019-02-21 00:28:18 -0500

    dictionary mode

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,9 +7,48 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+def normalize_text(text):
+    """
+    Given a text, lowercases it, removes all punctuation, 
+    and replaces all whitespace with normal spaces. Multiple whitespace will
+    be compressed into a single space.
+    """
+    text = text.casefold()
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
+
+    # Remove all punctuation
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+
+    text = new_text
+    text = text.replace("\n", " ")
+    return text
+
+
+def print_word_freq(filename):
+    """Read in `filename` and print out the frequency of words in that file."""
+    with open(filename) as file:
+        text = file.read()
+
+    text = normalize_text(text)
+    words = []
+    for word in text.split(" "):
+        if word != '' and word not in STOP_WORDS:
+            words.append(word)
+
+    ## What now?
+    # Get a dictionary of word frequencies and print it out
+    
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] = word_count[word] + 1
+        else:
+            word_count[word] = 1
+
+    print(word_count)
 
 
 if __name__ == "__main__":
